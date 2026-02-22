c38500dff9ac1912d0501c3d55a753e9826a405f
kyoh86 <me@kyoh86.dev>
2026-02-22 09:07:40 +0900

    allowlist更新を排他ロック+原子的置換に変更

--- cmd/mc-link-bot/internal/mclink/whitelist.go
+++ cmd/mc-link-bot/internal/mclink/whitelist.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"syscall"
 
 	"github.com/goccy/go-yaml"
 )
@@ -20,6 +21,20 @@ func AddAllowlistEntry(path string, typ EntryType, value string) error {
 	if value == "" {
 		return errors.New("value must not be empty")
 	}
+
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return err
+	}
+	lockPath := path + ".lock"
+	lockFile, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0o600)
+	if err != nil {
+		return err
+	}
+	defer lockFile.Close()
+	if err := syscall.Flock(int(lockFile.Fd()), syscall.LOCK_EX); err != nil {
+		return err
+	}
+	defer func() { _ = syscall.Flock(int(lockFile.Fd()), syscall.LOCK_UN) }()
 
 	cfg := Allowlist{}
 	if b, err := os.ReadFile(path); err == nil {
@@ -48,10 +63,30 @@ func AddAllowlistEntry(path string, typ EntryType, value string) error {
 	if err != nil {
 		return err
 	}
-	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+	tmpFile, err := os.CreateTemp(filepath.Dir(path), ".allowlist-*.yml")
+	if err != nil {
 		return err
 	}
-	return os.WriteFile(path, out, 0o644)
+	tmpPath := tmpFile.Name()
+	if _, err := tmpFile.Write(out); err != nil {
+		_ = tmpFile.Close()
+		_ = os.Remove(tmpPath)
+		return err
+	}
+	if err := tmpFile.Chmod(0o644); err != nil {
+		_ = tmpFile.Close()
+		_ = os.Remove(tmpPath)
+		return err
+	}
+	if err := tmpFile.Close(); err != nil {
+		_ = os.Remove(tmpPath)
+		return err
+	}
+	if err := os.Rename(tmpPath, path); err != nil {
+		_ = os.Remove(tmpPath)
+		return err
+	}
+	return nil
 }
 
 func containsFold(values []string, needle string) bool {
